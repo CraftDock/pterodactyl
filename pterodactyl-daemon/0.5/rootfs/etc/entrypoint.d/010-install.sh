21e37871bc9b19d25ccf7485984ba903d8d1ea69
Hexosse <hexosse@gmail.com>
2018-04-12 14:12:14 +0200

    Update permissions

--- pterodactyl-daemon/0.5/rootfs/etc/entrypoint.d/010-install.sh
+++ pterodactyl-daemon/0.5/rootfs/etc/entrypoint.d/010-install.sh
@@ -6,16 +6,23 @@ exec 2>&1
 # Make environnement variables accessibles
 [ -r /etc/envvars ] && . /etc/envvars
 
-# Make sure folders exist
-mkdir -p "/srv/daemon-data"
-
 # Create data, config, logs and packs folders
+mkdir -p "/srv/daemon-data"
 mkdir -p "/srv/daemon/config"
 mkdir -p "/srv/daemon/logs"
 mkdir -p "/srv/daemon/packs"
 
 # destroy link and recreate it
-mvlink /srv/daemon-data /daemon/data
 mvlink /srv/daemon/config /daemon/config
 mvlink /srv/daemon/logs /daemon/logs
 mvlink /srv/daemon/packs /daemon/packs
+
+# pterodactyl user used for server containers
+$(grep -q "^pterodactyl:" /etc/group) || addgroup -S pterodactyl
+$(grep -q "^pterodactyl:" /etc/passwd) || adduser -S -D -G pterodactyl pterodactyl
+
+# Adjust foders owner
+chown -R pterodactyl:pterodactyl /srv/daemon-data
+chown -R pterodactyl:pterodactyl /daemon/config
+chown -R pterodactyl:pterodactyl /daemon/logs
+chown -R pterodactyl:pterodactyl /daemon/packs
